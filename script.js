d984ee2b6b6f4389b7a1813d8af348c12e317e86
Macaylen <macaylen.exley@gmail.com>
2024-10-30 12:22:38 +0100

    logic re-factored

--- script.js
+++ script.js
@@ -1,56 +1,99 @@
 'use strict'
 
-let humanScore = 0;
-let computerScore = 0;
-let humanChoice = '';
-let computerChoice = '';
+    let humanScore = 0;
+    let computerScore = 0; 
+
+const playerChoiceDiv = document.querySelector('.player');
+const computerChoiceDiv = document.querySelector('.computer');
+const roundOne = document.querySelector('.round-one');
+const roundTwo = document.querySelector('.round-two');
+const roundThree = document.querySelector('.round-three');
+const roundFour = document.querySelector('.round-four');
+const roundFive = document.querySelector('.round-five');
+const finalResult = document.querySelector('.final-result');
 
 
 function playGame() {
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener('click', function(e){
-         const humanResult = e.target.textContent;
-         console.log(humanResult);
-            getComputerChoice();
-            
-    const checkRound =  function(computerChoice, humanChoice) {
-                let resultRound = '';
-                if        (computerChoice === 'Paper' && humanChoice === 'Rock') {
-                    let resultRound = 'lose';
-                    return resultRound;
-        } else if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
-            let resultRound = 'lose';
-            return resultRound;
-        } else if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
-            let resultRound = 'lose';
-            return resultRound;
-        } else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
-            let resultRound = 'win';
-            return resultRound;
-        } else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
-            let resultRound = 'win';
-            return resultRound;
-        } else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
-            let resultRound = 'win';
-            return resultRound;   
-        } else if (computerChoice === humanChoice) {
-            let resultRound = 'tie';
-            return resultRound;
-        } 
-        return resultRound;
-    }
-    // checkRound();
-    })
+            const humanResult = e.target.textContent;
+            console.log(`Player: ${humanResult}`);
+            const computerResult = getComputerChoice();
+            console.log(checkRound(computerResult, humanResult));
+            playerChoiceDiv.textContent = `Player's Choice: ${humanResult}`;
+            computerChoiceDiv.textContent = `Computer's Choice: ${computerResult}`;
+            let count = 0;
+            count++;
+            console.log(`Buttons clicked: ${count}`);
+        })
     })
         
 }
-    
-   
 
-
+function getComputerChoice() {
+            const res = parseInt(Math.random() * 3);
+            if (res === 0) {
+                console.log(`Computer: ${res} = Rock`);
+                return 'Rock'
+            } else if (res === 1) {
+                console.log(`Computer: ${res} = Paper`);
+                return 'Paper'
         
-        // while (humanScore < 5 && computerScore < 5) {   
+            } else if (res === 2) {
+                console.log(`Computer: ${res} = Scissors`);
+                return 'Scissors' }
+
+            console.log(res);
+            }
+        
+const checkRound =  function(computerChoice, humanChoice) {
+                let resultRound = '';
+                if        (computerChoice === 'Paper' && humanChoice === 'Rock') {
+                    let resultRound = 'You lose';
+                    computerScore++;
+                    console.log(computerScore);
+                    return resultRound;
+             } else if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
+            let resultRound = 'You lose';
+            computerScore++;
+            console.log(computerScore);
+            return resultRound;
+             } else if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
+            let resultRound = 'You lose';
+            computerScore++;
+            console.log(computerScore);
+            return resultRound;
+             } else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
+            let resultRound = 'You win';
+            humanScore++;
+            console.log(humanScore);
+            return resultRound;
+             } else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
+            let resultRound = 'You win';
+            humanScore++;
+            console.log(humanScore);
+            return resultRound;
+             } else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
+            let resultRound = 'You win';
+            humanScore++;
+            console.log(humanScore);
+            return resultRound;   
+             } else if (computerChoice === humanChoice) {
+            let resultRound = 'It\'s a tie';
+            return resultRound;
+        } 
+        checkScore();
+        console.log(resultRound);
+
+     
+    }
+        
+
+playGame();
+
+const checkScore = function() {
+ // while (humanScore < 5 && computerScore < 5) {   
                 
         // if (result === 'human') {
         //     humanScore++;
@@ -70,27 +113,4 @@ function playGame() {
         // } else {
         //     console.log("Computer wins the game! Better luck next time.");
         // }
-
-        function getComputerChoice() {
-            const res = Math.random();
-            console.log(res);
-            if (res > 0 && res < 0.33) {
-                return 'Rock'
-                // console.log("Computer: Rock")
-            } else if (res > 0.33 && res < 0.66) {
-                return 'Paper'
-                // console.log("Computer: Paper")
-        
-            } else (res > 0.66 && res < 0.99) 
-                return 'Scissors'
-                // console.log("Computer: Scissors")
-            }
-        
-        
-        
-
-
-
-
-
-playGame();
+}
